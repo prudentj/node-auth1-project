3fb103449bcd3b658e60d0eda847057b9e9f6e9b
Jacob Prudent <jake.prudent@gmail.com>
2020-02-25 14:42:21 -0800

    Almost MVP

--- api/api-router.js
+++ api/api-router.js
@@ -38,32 +38,45 @@ router.post('/login', (req, res) => {
 		.findBy({username})
 		.first()
 		.then(user => {
-			user && bcrypt.compareSync(password, user.password)
-				? res.status(200).json({message: 'Login Success'})
-				: res.status(401).json({message: 'You shall not pass!'});
+			if (user && bcrypt.compareSync(password, user.password)) {
+				req.session.user =
+					user && res.status(200).json({message: 'Login Success'});
+			} else {
+				res.status(401).json({message: 'You shall not pass!'});
+			}
 		})
 		.catch(error => {
 			res.status(500).json(error);
 		});
 });
+
+router.post('/logout', (req, res) => {
+	if (req.session) {
+		req.session.destroy(err => {
+			if (err) {
+				res.json({
+					message:
+						"When she calls you to go there is one thing you should know We don't have to live this way Baby, why don't you stay?"
+				});
+			} else {
+				res.json(200).json({message: 'You will be gentle on my mind'});
+			}
+		});
+	} else {
+		res
+			.status(200)
+			.json({
+				message:
+					'Whats your name whats your sign whats your birthday? You were never logged in'
+			});
+	}
+});
 //middleware
 function restricted(req, res, next) {
 	const username = req.header.username;
 	const password = req.header.password;
-	if (username && password) {
-		users
-			.findBy({username})
-			.first()
-			.then(user => {
-				if (user && bcrypt.compareSync(password, user.password)) {
-					next();
-				} else {
-					res.status(401).json({message: 'Invalid Username and password'});
-				}
-			})
-			.catch(({name, message, stack}) => {
-				res.status(500).json({name, message, stack});
-			});
+	if (req.session && req.session.user) {
+		next();
 	} else {
 		res.status(400).json({error: 'please provide credentials'});
 	}
--- api/server.js
+++ api/server.js
@@ -3,8 +3,29 @@ const server = express();
 
 const apiRouter = require('./api-router');
 const configMiddleware = require('./configure-middleware');
-
+const session = require('express-session');
+const KnexStore = require('connect-session-knex');
+const knex = require('../database/dbConfig');
+const sessionConfig = {
+	name: 'bloodstones', //sid
+	secret: 'keep it secret, keep it safe!',
+	cookie: {
+		maxAge: 1000 * 30,
+		secure: false, //true in production
+		httpOnly: true
+	},
+	resave: false,
+	saveUninitialized: true, //Need to be for production dynamic because of EU
+	store: new KnexStore({
+		knex,
+		tablename: 'sessions',
+		createtable: true,
+		sidfieldname: 'sid',
+		clearInterval: 1000 * 60 * 15
+	})
+};
 configMiddleware(server);
+server.use(session(sessionConfig));
 server.use('/api', apiRouter);
 
 module.exports = server;
